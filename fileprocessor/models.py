a1de6c75d71c7d601e24bc6c39496166a1937dd0
Marco Dantas <madbauru@gmail.com>
2023-07-31 00:51:25 -0300

    Alterações 6

--- fileprocessor/models.py
+++ fileprocessor/models.py
@@ -7,20 +7,16 @@ class Upload(models.Model):
         ('CO', 'Complete'),
     ]
 
-    file = models.FileField(upload_to='uploads/')
-    url = models.URLField(blank=True, null=True)
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='UP')
-    result = models.TextField(blank=True, null=True)
-   
     PRODUCT_CHOICES = [
         ('EB', 'E-book'),
         ('BP', 'Blog Post'),
         # Adicione mais opções aqui conforme necessário
     ]
 
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='UP')
     product = models.CharField(max_length=2, choices=PRODUCT_CHOICES, null=True)
-
-    class Upload(models.Model):
-    # ...
+    results = models.TextField(blank=True, null=True)
     ebook_title = models.CharField(max_length=200, blank=True, null=True)
-    # ...
+    blog_post_title = models.CharField(max_length=200, blank=True, null=True)
