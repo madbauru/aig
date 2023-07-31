9ae9f9b54184961b0e62b9aa57887af1c3ee7fb4
Marco Dantas <madbauru@gmail.com>
2023-07-31 00:12:42 -0300

    Alterações 2

--- fileprocessor/views.py
+++ fileprocessor/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Upload
 from django.http import HttpResponseRedirect
+import requests
+from bs4 import BeautifulSoup
+import textract
 
 def index(request):
     if request.method == 'POST':
@@ -22,10 +25,33 @@ def progress(request):
     # Se você estiver usando uma biblioteca de web scraping como Beautiful Soup ou Scrapy, você pode adicionar o código aqui
     # Se você estiver usando uma biblioteca de IA como TensorFlow ou PyTorch, você pode adicionar o código aqui
 
-    # Após o processamento, você pode salvar os resultados em um campo do modelo Upload
-    # Por exemplo:
-    # upload.results = results
-    # upload.save()
+    # Web scraping da URL
+    if upload.url:
+        response = requests.get(upload.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Remover cabeçalho, rodapé e barra lateral
+        for tag in soup.find_all(['header', 'footer', 'sidebar', 'nav', 'aside']):
+            tag.decompose()
+
+        # Extrair todo o texto do corpo
+        text = ' '.join(soup.body.stripped_strings)
+        upload.results = text
+        upload.save()
+
+    # Processar o arquivo
+    if upload.file:
+        # Ler o arquivo
+        file_content = textract.process(upload.file.path)
+
+        # Aqui você pode adicionar o código para processar o conteúdo do arquivo
+        # Por exemplo, se você estiver usando uma biblioteca de processamento de texto como NLTK ou SpaCy, você pode adicionar o código aqui
+        # Se você estiver usando uma biblioteca de IA como TensorFlow ou PyTorch, você pode adicionar o código aqui
+
+        # Após o processamento, você pode salvar os resultados em um campo do modelo Upload
+        # Por exemplo:
+        # upload.results = results
+        # upload.save()
 
     # Renderizar a página de progresso
     return render(request, 'progress.html')
